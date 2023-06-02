8e345437790fc198fbe1eff081541197ef2dfdc0
MARSHUKA <129719844+MARSHUKA@users.noreply.github.com>
2023-06-02 21:02:15 +0530

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Assignment3 {
+    public static void main(String[] args)  {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter your first number:");
+        int a=sc.nextInt();
+        System.out.println("Enter your second number:");
+        int b=sc.nextInt();
+
+        int sum = (a+b);
+        System.out.println("Sum of a and b is "  +sum);
+
+
+
+        
+    }
+    
+}
+
---
+++ Swapping.java
@@ -0,0 +1,14 @@
+public class Swapping {
+    public static void main(String [] args){
+
+        int a=10;
+        int b=20;
+        int c = a;
+        a=b;
+        b=c;
+
+        System.out.println("Swapping of a: " +a);
+        System.out.println("Swapping of b: " +c);
+    }
+    
+}
